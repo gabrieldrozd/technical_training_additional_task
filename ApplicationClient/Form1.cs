3bae40c5e41f4f2b22a7826c62be658d5142c06b
Gabriel <drozd.gj@gmail.com>
2022-05-02 16:34:27 +0200

    final version

---
+++ ApplicationClient/Form1.cs
@@ -0,0 +1,35 @@
+﻿using MathServiceLibrary;
+using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace ApplicationClient
+{
+    public partial class Form1 : Form
+    {
+        private readonly IMathService _mathClient = new ChannelFactory<IMathService>("MathService").CreateChannel();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _mathClient.Add(new MathServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+
+            int result = _mathClient.Substract(new MathServiceLibrary.MyNumbers() { Number1 = number1, Number2 = number2 });
+            textBox3.Text = result.ToString();
+        }
+    }
+}
---
+++ SelfHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using MathServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceSelfHostMathLibrary = null;
+
+            try
+            {
+                serviceSelfHostMathLibrary = new ServiceHost(typeof(MathService));
+                serviceSelfHostMathLibrary.Open();
+
+                Console.WriteLine("Hosting został uruchomiony");
+                Console.WriteLine("Serwis nasłuchuje żądań klientów na adresie:");
+                Console.WriteLine();
+
+                foreach (var serviceSelfHostEndpoint in serviceSelfHostMathLibrary.Description.Endpoints)
+                {
+                    Console.WriteLine($"Adres: {serviceSelfHostEndpoint.Address} Wiązanie: {serviceSelfHostEndpoint.Binding.Name}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Wciśnięcie dowolnego klawisza kończy pracę");
+                Console.ReadKey();
+
+                serviceSelfHostMathLibrary.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            finally
+            {
+                if (serviceSelfHostMathLibrary != null && serviceSelfHostMathLibrary.State == CommunicationState.Opened)
+                    serviceSelfHostMathLibrary.Close();
+            }
+        }
+    }
+}
